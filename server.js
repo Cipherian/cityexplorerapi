afd556c88df4c7aebfb10c13e60665f51a1afc18
Daniel Brott <danbrott42@gmail.com>
2022-08-20 09:12:37 -0700

    got backend weather cache and movie cache functioning

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 require("dotenv").config();
-const getMovies = require('./modules/Movies');
-const getWeather = require('./modules/Weather')
-const notFound = require('./modules/notFound');
+const movies = require('./modules/movies');
 const { response } = require("express");
+const getForecast = require("./modules/weather");
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 3002;
@@ -16,8 +15,28 @@ app.get("/", (request, response) => {
   response.send("testing");
 });
 
-app.get("/Weather", getWeather);
-app.get("/Movies", getMovies);
+app.get("/weather", getWeather);
+app.get("/movies", getMovies);
+
+function getWeather(request, response) {
+  const { lat, lon } = request.query;
+   getForecast(lat, lon)
+    .then((summaries) => response.send(summaries))
+    .catch((error) => {
+      console.error(error);
+      response.status(500).send('Sorry. Something went wrong!');
+    });
+}
+
+function getMovies(request, response) {
+  const cityMovie = request.query.cityMovie;
+  movies(cityMovie)
+    .then((summaries) => response.send(summaries))
+    .catch((error) => {
+      console.error(error);
+      response.status(500).send('Sorry. Something went wrong!');
+    });
+}
 
 app.use((error, request, response, next) => {
   console.log(error);
